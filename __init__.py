36a42ef771a88e6a2e49ab5edee282015c3d881f
bstocker <boris.stocker@gmail.com>
2024-03-08 11:55:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,17 +5,17 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'mysql-etudiant.alwaysdata.net'
-app.config['MYSQL_USER'] = 'etudiant'
+app.config['MYSQL_HOST'] = 'mysql-etudiant2.alwaysdata.net'
+app.config['MYSQL_USER'] = 'etudiant2'
 app.config['MYSQL_PASSWORD'] = 'EPSI2024'
-app.config['MYSQL_DB'] = 'etudiant_clients'
+app.config['MYSQL_DB'] = 'etudiant2_clients'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def accueil():  
     cursor = mysql.connection.cursor()
-    cursor.execute('SELECT id, nom, prenom FROM client')
+    cursor.execute('SELECT id, nom, prenom FROM clients')
     data = cursor.fetchall()
     mysql.connection.commit()
     cursor.close()
