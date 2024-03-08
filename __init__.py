139cc8635ab4fde1121a523dbdcb2418bb97d372
bstocker <boris.stocker@gmail.com>
2024-03-08 11:35:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'mysql-etudiant.alwaysdata.net'
+app.config['MYSQL_USER'] = 'etudiant'
+app.config['MYSQL_PASSWORD'] = 'EPSI2024'
+app.config['MYSQL_DB'] = 'etudiant_clients'
+
+mysql = MySQL(app)
+
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
-                                                                                                                                       
+def accueil():  
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT id, nom, prenom FROM client')
+    data = cursor.fetchall()
+    mysql.connection.commit()
+    cursor.close()
+    return render_template('lectureBDD.html', data=data)
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
